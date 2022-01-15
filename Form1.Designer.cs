3f289946f8a8916a6a982a56f96832160586051c
Павел Карев <karevpavel133@gmail.com>
2022-01-15 13:05:08 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace JavaSwapper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.shortName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.jdkPath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gridBtn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.dataGridView1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(4, 4);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(584, 191);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(89, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 26);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Сохранить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.shortName,
+            this.jdkPath,
+            this.gridBtn});
+            this.dataGridView1.GridColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 35);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(574, 154);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // shortName
+            // 
+            this.shortName.HeaderText = "Название";
+            this.shortName.MinimumWidth = 6;
+            this.shortName.Name = "shortName";
+            this.shortName.ReadOnly = true;
+            this.shortName.Width = 125;
+            // 
+            // jdkPath
+            // 
+            this.jdkPath.HeaderText = "Путь";
+            this.jdkPath.MinimumWidth = 6;
+            this.jdkPath.Name = "jdkPath";
+            this.jdkPath.ReadOnly = true;
+            this.jdkPath.Width = 125;
+            // 
+            // gridBtn
+            // 
+            this.gridBtn.HeaderText = "";
+            this.gridBtn.MinimumWidth = 6;
+            this.gridBtn.Name = "gridBtn";
+            this.gridBtn.ReadOnly = true;
+            this.gridBtn.Text = "Удалить";
+            this.gridBtn.Width = 125;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "ТУТ ТЕКСТ";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 202);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn shortName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn jdkPath;
+        private System.Windows.Forms.DataGridViewButtonColumn gridBtn;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace JavaSwapper
+{
+    public partial class Form1 : Form
+    {
+        private static readonly string FILE_NAME = "TMP_JDK_LIST.txt";
+        private static readonly string ABSOLUTE_FILE_PATH = System.IO.Path.GetTempPath() + FILE_NAME;
+        private static readonly string JAVA_HOME = "JAVA_HOME";
+        private static readonly string PATH = "Path";
+        public static Dictionary<string, string> jdkKvp = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadDictionaryFromFile();
+            FUCK_C_SHARP();
+        }
+
+        private void loadDictionaryFromFile()
+        {
+            if (System.IO.File.Exists(ABSOLUTE_FILE_PATH))
+            {
+                string[] jdkLines = System.IO.File.ReadAllLines(ABSOLUTE_FILE_PATH);
+                foreach (string jdkLine in jdkLines)
+                {
+                    var regex = new System.Text.RegularExpressions.Regex("\"(.*)\"\\:\"(.*)\"");
+                    var match = regex.Match(jdkLine);
+                    if (match.Success)
+                    {
+                        jdkKvp.Add(match.Groups[1].Value, match.Groups[2].Value);
+                    }
+                }
+            }
+        }
+
+        private void FUCK_C_SHARP()
+        {
+            this.dataGridView1.CellClick += this.CellClick;
+            this.Resize += Form1_Resize;
+            this.gridBtn.Text = "Удалить";
+            this.gridBtn.UseColumnTextForButtonValue = false;
+
+            foreach (var kv in jdkKvp)
+            {
+                this.insertValueToDG(kv.Key, kv.Value);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.notifyIcon1.ContextMenu = new System.Windows.Forms.ContextMenu();
+                this.notifyIcon1.ContextMenu.MenuItems.Add(new System.Windows.Forms.MenuItem("Развернуть",
+                    new System.EventHandler((object objc, System.EventArgs args) => { this.WindowState = System.Windows.Forms.FormWindowState.Normal; })));
+
+                foreach (var kvp in JavaSwapper.Form1.jdkKvp)
+                {
+                    this.notifyIcon1.ContextMenu.MenuItems.Add(new System.Windows.Forms.MenuItem(kvp.Key,
+                         new System.EventHandler((object objc, System.EventArgs args) => {
+                             var currentJavaHome = System.Environment.GetEnvironmentVariable(JAVA_HOME, EnvironmentVariableTarget.Machine);
+                             var newJavaHome = kvp.Value;
+                             var currentPath = System.Environment.GetEnvironmentVariable(PATH, EnvironmentVariableTarget.Machine);
+                             var newPath = currentPath.Replace(String.Format("{0}\\bin;", currentJavaHome), String.Format("{0}\\bin;", newJavaHome));
+                             System.Environment.SetEnvironmentVariable(PATH, newPath, System.EnvironmentVariableTarget.Machine);
+                             System.Environment.SetEnvironmentVariable(JAVA_HOME, newJavaHome, System.EnvironmentVariableTarget.Machine);
+                         })));
+                }
+            }
+        }
+
+        private string javaVersion(string jdkPath)
+        {
+            var cmd = new System.Diagnostics.Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.RedirectStandardError = true;
+            cmd.StartInfo.CreateNoWindow = false;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.Start();
+            cmd.StandardInput.WriteLine(String.Format("\"{0}\\bin\\java\" -version", jdkPath));
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            string result = cmd.StandardError.ReadToEnd();
+            cmd.WaitForExit();
+            var regex = new System.Text.RegularExpressions.Regex("(\\w+)\\sversion\\s\"(.*)\"");
+            var match = regex.Match(result);
+            if (match.Success)
+            {
+                var JdkName = match.Groups[1].Value;
+                var firstElement = JdkName.ElementAt(0);
+                JdkName = JdkName.Replace(firstElement, Char.ToUpper(firstElement));
+                var jdkVersion = match.Groups[2].Value;
+                return String.Format("{0} {1}", JdkName, jdkVersion);
+            }
+            return "idianahooi;";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dialogResult = this.folderBrowserDialog1.ShowDialog();
+            dialogResult.ToString();
+            if (dialogResult == DialogResult.OK)
+            {
+                var jdkPath = this.folderBrowserDialog1.SelectedPath;
+                var shortName = this.javaVersion(jdkPath);
+                jdkKvp.Add(shortName, jdkPath);
+                this.insertValueToDG(shortName, jdkPath);
+            }
+        }
+
+        private void saveToFile()
+        {
+            var fileName = System.IO.Path.GetTempPath() + FILE_NAME;
+            List<string> content = new List<string>();
+            foreach (var kvp in jdkKvp)
+            {
+                content.Add(String.Format("\"{0}\":\"{1}\"", kvp.Key, kvp.Value));
+            }
+            System.IO.File.WriteAllLines(fileName, content);
+        }
+
+        private void insertValueToDG(string shortName, string jdkPath)
+        {
+            var rowIndex = this.dataGridView1.Rows.Count;
+            this.dataGridView1.Rows.Insert(rowIndex, shortName, jdkPath, "Удалить");
+        }
+
+        private void CellClick(object sender, System.Windows.Forms.DataGridViewCellEventArgs e)
+        {
+            if (e.ColumnIndex == this.dataGridView1.Columns.Count - 1)
+            {
+                jdkKvp.Remove(this.dataGridView1.Rows[e.RowIndex].Cells["shortName"].Value.ToString());
+                this.dataGridView1.Rows.RemoveAt(e.RowIndex);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (jdkKvp.Count > 0)
+            {
+                saveToFile();
+            }
+        }
+    }
+}
